b277bdbcc2956315e3850c84d5183b77dccaad67
Sweet Van Loan <sweet@sweetvanloan.com>
2020-01-28 23:40:13 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -61,7 +61,7 @@ end
 
   def turn
     puts "Please enter 1-9:"
-    input = gets.strip
+    input = gets.to_i
     index = input_to_index(input)
     if valid_move?(board, index)
       move(board, index)
